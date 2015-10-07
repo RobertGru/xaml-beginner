f6a073adb981d2482adf53a813b0096055e6227b
RobertGru <robertgrutza@gmail.com>
2015-10-07 07:45:06 -0500

    F# Module 2 Problems and Homework
    F# module 2 homework

---
+++ Fsharp_Conditionals/Fsharp_Conditionals/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let checksum v = 
+    if v = 5 then "I am 5"
+    elif v < 5 then "I am less than 5"
+    else " I am another number"
+
+let checksum2 t = 
+    if t = 5 || t > 7 then "valid check"
+    
+    else "not in range"
+
+
+[<EntryPoint>]
+let main argv = 
+    let check = Console.ReadLine()
+    let check = int check
+    printfn "%A" (checksum check)
+    Console.ReadKey()
+    0 // return an integer exit code
---
+++ Fsharp_Conditionals2/Fsharp_Conditionals2/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+
+let checkvalue (argv : string []) : int =
+    if argv.Length > 0 then      
+        let couldparse, consolein = Int32.TryParse(argv.[0])
+        if couldparse then consolein
+        else
+            let canparse, keyin = Int32.TryParse(Console.ReadLine())
+            if canparse then keyin
+            else 0
+    else
+        let canparse, keyin = Int32.TryParse(Console.ReadLine())
+        if canparse then keyin
+        else 0
+
+let fibonacci n =
+    let mutable first = 0
+    let mutable second = 1
+    let mutable temp = 0
+    for index = 1 to n do
+        temp <- first + second
+        first <- second
+        second <- temp
+    first
+
+let rec fibonaccirecursive n =
+    if n <= 2 then 1
+        elif n = 0 then 0
+        else
+            fibonaccirecursive (n - 1) + fibonaccirecursive (n - 2)
+
+[<EntryPoint>]
+let main argv =
+    let input = checkvalue argv
+//    let calc = fibonacci input
+    let calc2 = fibonaccirecursive input
+//    Console.WriteLine(calc)
+    Console.WriteLine(calc2)
+    Console.ReadKey()
+    0 // return an integer exit code
